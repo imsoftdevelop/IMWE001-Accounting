960d302520d6ee67cd72e147393209ac3af9d16e
IM.Soft <im.softdevelopment@gmail.com>
2020-06-03 20:37:22 +0700

    First

---
+++ IMWE001-Accounting/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IMWE001_Accounting.Models;
+
+namespace IMWE001_Accounting.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IMWE001-Accounting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IMWE001_Accounting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
