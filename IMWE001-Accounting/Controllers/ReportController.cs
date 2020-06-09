dea7e482102315b99437e8fed0ffbded080e866f
IM.Soft <im.softdevelopment@gmail.com>
2020-06-09 18:58:31 +0700

    G

---
+++ IMWE001-Accounting/Controllers/ReportController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IMWE001_Accounting.Models;
+
+namespace IMWE001_Accounting.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Reportsales()
+        {
+            return View();
+        }
+
+        public IActionResult Reportsalebycustomer()
+        {
+            return View();
+        }
+
+        public IActionResult Reportbillingnote()
+        {
+            return View();
+        }
+
+        public IActionResult Reportreceivables()
+        {
+            return View();
+        }
+
+        public IActionResult Reportpurchaseorder()
+        {
+            return View();
+        }
+
+
+        public IActionResult Reportreceivinginventory()
+        {
+            return View();
+        }
+
+        public IActionResult Reportexpense()
+        {
+            return View();
+        }
+        public IActionResult Reportsalestax()
+        {
+            return View();
+        }
+
+        public IActionResult Reportpurchasevat()
+        {
+            return View();
+        }
+
+        public IActionResult Reportwithholdingtax()
+        {
+            return View();
+        }
+
+        public IActionResult Reportaccountreceivable()
+        {
+            return View();
+        }
+
+        public IActionResult Reportaccountpayable()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IMWE001-Accounting/wwwroot/lib/jquery.toast/toastDemo.js
@@ -0,0 +1,33 @@
+﻿(function ($) {
+    showWariningToast = function () {
+        resetToastPosition();
+        $.toast({
+            heading: '<i class=" icon-info jq-iconcol-warning px-2 py-1"></i> ไม่สามารถบันทึกได้ เนื่องจากข้อมูลไม่ถูกต้อง/ไม่ครบถ้วน',
+            text: '<p class="jq-toast-text">1. กรุณาเลือกผู้ติดต่อ</p>',
+            showHideTransition: 'slide',
+            icon: 'warning',
+            loaderBg: '#ffcf55',
+            position: 'top-center'
+        })
+    };
+    showSuccessToast = function () {
+        resetToastPosition();
+        $.toast({
+            heading: '<i class= "fa fa-check-circle jq-iconcol-success px-2 py-1" ></i> บันทึกข้อมูลสำเร็จ',
+            text: '',
+            showHideTransition: 'slide',
+            icon: 'success',
+            loaderBg: '#46c35f',
+            position: 'top-center'
+        })
+    };
+    resetToastPosition = function () {
+        $('.jq-toast-wrap').removeClass('bottom-left bottom-right top-left top-right top-center'); // to remove previous position class
+        $(".jq-toast-wrap").css({
+            "top": "",
+            "left": "",
+            "bottom": "",
+            "right": ""
+        }); //to remove previous position style
+    }
+})(jQuery);
